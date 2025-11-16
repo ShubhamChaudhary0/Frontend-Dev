e0b07c0c24685331ebcb7d80898ca2aee5209395
ShubhamChaudhary0 <chaudharyshubham1947@gmail.com>
2025-11-16 20:02:40 +0530

    complete

--- js_operators/Q5.js
+++
@@ -1,6 +0,0 @@
-let isDoorLocked = true;
-let isWindowClosed = true;
-let isAlarmOn = true;
-let isOwnerInside = true;
-
-console.log(isDoorLocked && isWindowClosed && isAlarmOn && isOwnerInside ? "Secure" : "Unsafe");
