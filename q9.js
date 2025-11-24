76255c28266919f636288802d65e809d37421ff4
ShubhamChaudhary0 <chaudharyshubham1947@gmail.com>
2025-11-24 10:42:25 +0530

    assignment done

---
+++ q9.js
@@ -0,0 +1,23 @@
+// ES6 Version
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    showName() {
+        console.log("Name:", this.name);
+    }
+}
+
+class Student extends Person {
+    constructor(name, branch) {
+        super(name);
+        this.branch = branch;
+    }
+    showBranch() {
+        console.log("Branch:", this.branch);
+    }
+}
+
+const s1 = new Student("Yash", "CSE");
+s1.showName();
+s1.showBranch();
